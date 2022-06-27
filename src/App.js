4057cc282a85e7343b44dbae125da03f02564e33
www.SharibSaifi.SS@gmail.com <www.SharibSaifi.SS@gmail.com>
2022-06-27 15:31:51 +0530

    TextCorrector

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+
+import TextForm from "./Components/TextForm";
+import Navbar from "./Components/Navbar";
+import Alert from "./Components/Alert";
+import React,{useState} from "react";
+
+
 
 function App() {
+  const [mode,setDarkmode] = useState('light');
+  const [alert, setAlert] = useState(null);
+  
+
+//Alert Function
+  const showAlert =(message,type) =>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    },1000)
+  }
+//Mode (Dark or Light)  
+  const toggleMode = () =>{
+    if(mode === 'light'){
+      setDarkmode('dark');
+      document.body.style.backgroundColor = 'black';
+      document.body.style.color = 'white';
+      showAlert("Dark mode Enables","success");
+    }
+    else{
+      setDarkmode('light');
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+      showAlert("Light mode Enables","success");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   
+      <Navbar title="TextCorrector" mode={mode} enableDarkMode={toggleMode} about = "About Site"  />
+      <Alert alert={alert}/>
+      <div className="container">
+       
+         
+            <TextForm heading = " Enter the text for Analyse" showAlert={showAlert}/>
+          
+      </div>
+    </>
+    
   );
 }
-
 export default App;
+
---
+++ src/Components/TextForm.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+
+
+export default function TextForm(props) {
+  
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to UpperCase","success");
+  };
+  const LowerCase = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to LowerCase","success");
+  };
+  const Clear = () =>{
+    setText('');
+    props.showAlert("Cleared","success");
+  }
+  const Copy = () => {
+    let text = document.getElementById("myBox");
+    text.select();
+    //Copying the text to clipboard
+    navigator.clipboard.writeText(text.value);
+    props.showAlert("Copied to Clipboard","success");
+
+  }
+  //ye necessary h iski madad se hi hmm apne textarea m value le sakte h
+  const handleOnchange = (event) => {
+    // console.log("handle on change");
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("");
+  // setText("new text");
+  return (
+    <>
+      <div className="Container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            style={{backgroundColor: props.mode === 'light'?'grey':'light'}}
+            value={text}
+            onChange={handleOnchange}
+            id="myBox"
+            rows="8"
+            placeholder="Enter" 
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+          Convert To UpperCase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={LowerCase}>
+          Convert To LowerCase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={Clear}>
+          Clear TextArea
+        </button>
+        <button className="btn btn-primary mx-2" onClick={Copy}>
+          CopyText
+        </button>
+      </div>
+      <div className="Container my-3">
+        <h2>Your Text Summary</h2>
+        <p>Your Para has {text.split(" ").length} Words and {text.length} Characters</p>
+        <p>Spaces : {text.split(" ").length}</p>
+        <p>{0.008 * text.split(" ").length} Minutes Read</p>
+        <h2>Preview</h2>
+        <p>{text.length >0?text:"Enter Something"}</p>
+      </div>
+    </>
+  );
+}
